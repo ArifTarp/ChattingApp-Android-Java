aea82b975a480ee59bf7b542bfb349a4f2442cf0
ArifTarp <arifoklh@gmail.com>
2021-03-20 21:19:34 +0300

    Added verify phoneNumber with firebase

--- app/src/main/java/com/apps/chattingapp/PhoneNumberActivity.java
+++ app/src/main/java/com/apps/chattingapp/PhoneNumberActivity.java
@@ -2,13 +2,31 @@ package com.apps.chattingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.apps.chattingapp.databinding.ActivityPhoneNumberBinding;
 
 public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number);
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhoneNumberActivity.this, OTPActivity.class);
+                intent.putExtra("phoneNumber", binding.phoneBox.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
