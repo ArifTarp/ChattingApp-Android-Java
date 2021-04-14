152c7039885484f1a1ef07cb0e6b2401009c4b7c
ArifTarp <arifoklh@gmail.com>
2021-04-15 00:30:22 +0300

    Added users adapter and conversation operations

--- app/src/main/java/com/apps/chattingapp/MainActivity.java
+++ app/src/main/java/com/apps/chattingapp/MainActivity.java
@@ -8,12 +8,49 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.apps.chattingapp.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseDatabase database;
+    ArrayList<User> users;
+    UsersAdapter usersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        database = FirebaseDatabase.getInstance();
+        users = new ArrayList<>();
+
+        usersAdapter = new UsersAdapter(this, users);
+        binding.recyclerView.setAdapter(usersAdapter);
+
+        database.getReference().child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for (DataSnapshot ss : snapshot.getChildren()) {
+                    User user = ss.getValue(User.class);
+                    users.add(user);
+                }
+                usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
