5ccb1c74e84ac39985f046fcc93c1f0ec181fe0a
ArifTarp <arifoklh@gmail.com>
2021-04-18 22:37:50 +0300

    Added message adapter

--- app/src/main/java/com/apps/chattingapp/Activities/ChatActivity.java
+++ app/src/main/java/com/apps/chattingapp/Activities/ChatActivity.java
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.apps.chattingapp.Adapters.MessagesAdapter;
+import com.apps.chattingapp.Models.Message;
 import com.apps.chattingapp.databinding.ActivityChatBinding;
+
+import java.util.ArrayList;
 
 public class ChatActivity extends AppCompatActivity {
 
     ActivityChatBinding binding;
+    MessagesAdapter adapter;
+    ArrayList<Message> messages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        messages = new ArrayList<>();
+        adapter = new MessagesAdapter(this,messages);
 
         String name = getIntent().getStringExtra("name");
         String uid = getIntent().getStringExtra("uid");
